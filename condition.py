43254c7a11e7aad7af3d290eb46a8a03c8d28722
anushamathad <anushamathad92@gmail.com>
2021-12-10 15:42:31 +0530

    My First commit to push to cloud repo

---
+++ condition.py
@@ -0,0 +1,10 @@
+# if condition
+
+number = int(input("enter any number of your choice:-"))
+
+if number < 0:
+    print("number is less than zero")
+elif number ==0:
+    print("number is equal to zero")
+else:
+    print("number is more than zero")
