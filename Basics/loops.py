576ca9d14274fce28050d4c32ea1e3bb6db59e7e
anushamathad <anushamathad92@gmail.com>
2022-02-16 22:45:06 +0530

    init commit

---
+++ Basics/loops.py
@@ -0,0 +1,31 @@
+
+def main():
+    x = 0
+    
+    # defining a while loop
+    while (x < 5):
+        print(x)
+        x = x + 1
+
+    # defining a for loop
+    for x in range(5,10):
+        print (x)
+      
+    # using a for loop over a collection
+    days = ["Mon","Tue","Wed","Thu","Fri","Sat","Sun"]
+    for d in days:
+        print (d)
+    
+    # usage of break and continue statements
+    for x in range(5,10):
+        #if (x == 7): break
+        #if (x % 2 == 0): continue
+        print (x)
+    
+    # using the enumerate() function to get index 
+    days = ["Mon","Tue","Wed","Thu","Fri","Sat","Sun"]
+    for i, d in enumerate(days):
+        print (i, d)
+  
+if __name__ == "__main__":
+    main()
